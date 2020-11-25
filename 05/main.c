d674e608a794b0d523e137597ce0b07c31557ce8
Ilya Nikolskiy <aspdaspn@gmail.com>
2020-11-25 14:56:46 +0100

    Empty Homework 05

---
+++ 05/main.c
@@ -0,0 +1,15 @@
+/* 
+* Algorithmus and data structures
+* Homework 05
+* by Ilya Nikolskiy
+* 
+* For all checking persons: this file was saved in UTF-8 encoding.
+* Please consider this if Russian characters will be displayed incorrectly.
+*/
+
+#include <stdio.h>
+
+// TBD
+int main() {
+	return 0;
+}
