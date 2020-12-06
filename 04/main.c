7f412e700f62d9df758cd373813798dcb641cb11
Ilya Nikolskiy <aspdaspn@gmail.com>
2020-12-06 22:59:42 +0100

    Tried to solve Knihgt problem

--- 04/main.c
+++ 04/main.c
@@ -27,14 +27,62 @@ void print2(int n, int m, int a[n][m]) {
 		}
 }
 
+int can_move(int x, int y, int b[8][8]) {
+	if (x < 1 || x > 7 || y < 0 || y > 7) {
+		printf("cannot move! x = %d y = %d\n", x, y);
+		return 0;
+	}
+	else if (b[x][y] != 0) {
+		printf("not empty cell! x = %d y = %d, value = %d\n", x, y, b[x][y]);
+		return 0;
+	}
+	return 1;
+}
+
+int next_move(int x, int y, int b[8][8], int m[8][2], int move) {
+	int i;
+	int newX, newY;
+	if (move > 64)
+		return 0;
+	else {
+		b[x][y] = move;
+		printf("%d %d %d %d\n", move, x, y, b[x][y]);
+		for (i = 0; i < 8; i++) {
+			newX = x + m[i][0];
+			newY = y + m[i][1];
+			if (can_move(newX, newY, b) && next_move(newX, newY, b, m, move + 1)) {
+				b[x][y] = move;
+				return move;
+			} else {
+				b[x][y] = 0;
+			}
+		}
+	}
+}
+void knight_move() {
+	int board[8][8] = {{0, 0, 0, 0, 0, 0, 0, 0},
+										 {0, 0, 0, 0, 0, 0, 0, 0},
+									 	 {0, 0, 0, 0, 0, 0, 0, 0},
+										 {0, 0, 0, 0, 0, 0, 0, 0},
+										 {0, 0, 0, 0, 0, 0, 0, 0},
+										 {0, 0, 0, 0, 0, 0, 0, 0},
+										 {0, 0, 0, 0, 0, 0, 0, 0},
+										 {0, 0, 0, 0, 0, 0, 0, 0}};
+
+	int moves[8][2] = {{-1, -2}, {-2, -1}, {-2, 1}, {1, -2},
+										 {-1,  2}, { 2, -1}, { 1, 2}, {2,  1}};
+	next_move(0, 0, board, moves, 1);
+	print2(8, 8, board);
+}
+
 int main() {
 	int map[3][3] = {{1, 1, 1},
 									 {0, 1, 0},
 									 {0, 1, 0}};
 	int way[3][3];
-	int sequence[10][11];
-	char first[11] = {'g','e','e','k','b','r','a','i','n','s'};
-	char second[11] = {'g','e','e', 'k', 'm', 'i', 'n', 'd', 's'};
+	int sequence[10][11];	//row | column
+	char first[11] = {'g','e','e','k','b','r','a','i','n','s','\0'};
+	char second[10] = {'g','e','e','k','m','i','n','d','s','\0'};
 	int i, j, k;
 	int selector = 0;
 	do {
@@ -63,34 +111,24 @@ int main() {
 				print2(3, 3, way);
 				break;
 			case 2:
-			/*
-			* что-то не работает цикл k++, идея в том, что если символы совпали,
-			* заполнить строку предыдущим значением на этой строке.
-			*/
-				for (j = 0; j < 11; j++)	//Horizontal
+				for (j = 0; j < 11; j++)
 					sequence[0][j] = 0;
-				for (i = 0; i < 10; i++)	//Vertical
+				for (i = 0; i < 10; i++)
 					sequence[i][0] = 0;
 				for (i = 1; i < 10; i++)
 					for (j = 1; j < 11; j++)
-						if (first[j - 1] == second[i - 1]) {
-							sequence[i][j] = sequence[i - 1][j] + 1;
-							for (k = j; k < 10; k++)
-								sequence[i][k + 1] = sequence[i][k];
-							printf("Match %d %d\n", i, j);
-							print2(10, 11, sequence);
-						} else {
-//							sequence[i][j] = sequence[i - 1][j];
-							sequence[i][j] = sequence[i][j - 1];
-							printf("Not match %d %d\n", i, j);
-							print2(10, 11, sequence);
-						}
+						if (first[j - 1] == second[i - 1])
+							sequence[i][j] = sequence[i - 1][j - 1] + 1;
+						else 
+							if (sequence[i][j - 1] >= sequence[i - 1][j])
+								sequence[i][j] = sequence[i][j - 1];
+							else
+								sequence[i][j] = sequence[i - 1][j];
 				print2(10, 11, sequence);
+				printf("Максимальная длина последовательностей %s и %s: %d\n", first, second, sequence[9][10]);
 				break;
 			case 3:
-			/*
-			* интересная задача, но не успеваю, сделаю позже
-			*/
+				knight_move();
 				break;
 			case 0:
 				break;
